38bf40a72c5894651c24c2bf80af3654eca5335f
ElAl3x104 ! <bonura.alex@iisgalvanimi.edu.it>
2025-05-22 07:33:29 +0000

    terzo punto

--- bonny/src/app/app.component.ts
+++ bonny/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Canzoni } from './model/song.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  Canzoni(Canzoni: any) {
+    throw new Error('Method not implemented.');
+  }
   title = 'bonny';
+
+
+  vettCanzoni : Canzoni[] = [];
+  http: HttpClient;
+  loading: boolean = false;
+  o!: Observable<Canzoni[]>;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+    this.loading = true;
+    this.o = this.http.get<Canzoni[]>('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist');
+    this.o.subscribe(this.getData);
+  }
+  getData = (d: Canzoni[]) => {
+    this.vettCanzoni = d;
+    console.log(this.vettCanzoni)
+    this.loading = false;
+  }
 }
